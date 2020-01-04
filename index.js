316b0fe7637108c64ea249d777b023f216e8b294
Dane Maison <danemaison@gmail.com>
2020-01-03 20:17:46 -0800

    Updated html: Removed duplicate content, removed mobile/desktop specific nav bars, added more sematic tags, added links to socia media icons, logos, and nav items Css: Updated styles to more accurately reflect PSD, updated hamburger menu, text shadow, team section header, etc. JS: Removed variable from global scope, re-wrote random selection algorithm, removed style manipulation from JS, added moved script to the bottom of html doc

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", applyClickHandlers);
-let imagesFolder = null;
 
 function init() {
   Promise.all([
@@ -22,64 +21,32 @@ function init() {
 }
 
 function loadProfileCards(employeeData) {
-  const names = employeeData[0];
-  const images = employeeData[1];
-  const descriptions = employeeData[2];
-  imagesFolder = images["images-folder"];
-  const employeeCount = names.employees.length;
-  const ids = [];
-  do {
-    const rand = Math.floor(Math.random() * employeeCount);
-    if (!ids.includes(rand)) {
-      ids.push(rand);
-      renderCard(
-        names.employees[rand],
-        images.employees[rand],
-        descriptions.employees[rand]
-      );
-    }
-  } while (ids.length < 3);
-}
-
-function renderCard(name, image, description) {
   const cards = document.querySelectorAll(".profile-card");
+  const wrappers = document.querySelectorAll(".wrapper");
+  const imagesFolder = employeeData[1]["images-folder"];
+  const names = employeeData[0].employees;
+  const images = employeeData[1].employees;
+  const descriptions = employeeData[2].employees;
+  const randomizerList = employeeData[0].employees.slice();
 
-  for (let i = 0; i < cards.length; i++) {
-    const classList = cards[i].classList;
-    const style = cards[i].style;
-    const children = cards[i].children;
-    if (!classList.contains("loaded")) {
-      const fullName = name.first_name + " " + name.last_name;
-      const title = description.title;
-      style.backgroundImage = "url(" + imagesFolder + image.full + ")";
-
-      if (fullName.toLowerCase() === "michelangelo buonarroti") {
-        style.boxShadow = "inset 0 0 0 200px rgba(0,0,0,.25)";
-      }
-      if (fullName.length > 20) {
-        children[0].style.fontSize = "18px";
-      } else if (fullName.length > 15) {
-        children[0].style.fontSize = "22px";
-      }
-      if (title.length > 37) {
-        children[1].style.fontSize = "12px";
-      }
-
-      children[0].innerText = fullName;
-      children[1].innerText = title;
-      children[2].innerText = description.description;
-
-      classList.add("loaded");
-
-      break;
-    }
+  for (let i = 0; i < 3; i++) {
+    const rand = Math.floor(Math.random() * randomizerList.length);
+    const index = randomizerList.splice(rand, 1)[0].id - 1;
+    const children = wrappers[i].children;
+    children[0].innerText =
+      names[index].first_name + " " + names[index].last_name;
+    children[1].innerText = descriptions[index].title;
+    children[2].innerText = descriptions[index].description;
+    cards[i].style.backgroundImage =
+      "url(" + imagesFolder + images[index].full + ")";
   }
 }
+
 function applyClickHandlers() {
-  const mobileLinks = document.querySelectorAll(".mobile-link");
-  mobileLinks.forEach(function(link) {
+  const links = document.querySelectorAll("nav .link");
+  links.forEach(function(link) {
     link.addEventListener("click", function(e) {
-      mobileLinks.forEach(function(link) {
+      links.forEach(function(link) {
         link.classList.remove("active");
       });
       e.currentTarget.classList.add("active");
@@ -94,5 +61,5 @@ function applyClickHandlers() {
 
 function toggleMobileMenu() {
   document.querySelector(".hamburger").classList.toggle("open");
-  document.querySelector(".mobile-nav").classList.toggle("active");
+  document.querySelector(".links").classList.toggle("open");
 }
